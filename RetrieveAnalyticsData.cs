b592c3cdd6b3182df0fc7d406c033daf717fb52f
oliver-tbs <113384797+oliver-tbs@users.noreply.github.com>
2024-11-08 11:08:09 -0500

    Modelling update, very WIP

--- RetrieveAnalyticsData.cs
+++ RetrieveAnalyticsData.cs
@@ -1,4 +1,4 @@
-using Google.Api;
+using appsvc_function_dev_cm_stats_dotnet001.Configuration;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
@@ -6,9 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Azure.Storage.Blobs;
-using log4net.Repository.Hierarchy;
 using Newtonsoft.Json.Linq;
-
 
 namespace appsvc_function_dev_cm_stats_dotnet001
 {
@@ -24,30 +22,49 @@ namespace appsvc_function_dev_cm_stats_dotnet001
 
 
 
-
+        // dateHour = e.DimensionValues[0].Value;
+        // eventName = e.DimensionValues[1].Value;
+        // linkUrl = e.DimensionValues[2].Value;
 
 
         private class EventRow
         {
             public List<MyValue> DimensionValues;
             //public List<MyValue> MetricValues;
+
+            public string DateHour
+            {
+                get { return DimensionValues[0].Value; }
+            }
+
+            public string EventName
+            {
+                get { return DimensionValues[1].Value; }
+            }
+
+            public string LinkUrl
+            {
+                get { return DimensionValues[2].Value; }
+            }
+
+            public string Id
+            {
+                get { return LinkUrl.Substring(LinkUrl.LastIndexOf("?ID=") + 4); }
+            }
         }
 
         private class MyValue
         {
-
             [JsonProperty(PropertyName = "Value")]
             public string Value;
-            //public bool HasValue;
-            //public int OneValueCase;
         }
 
-        private class GoogleAnalyticsEvent {
+        private class Event {
             public string DateHour;
             public string EventName;
             public string LinkUrl;
 
-            public GoogleAnalyticsEvent(string dateHour, string eventName, string linkUrl)
+            public Event(string dateHour, string eventName, string linkUrl)
             {
                 DateHour = dateHour;
                 EventName = eventName;
@@ -55,11 +72,15 @@ namespace appsvc_function_dev_cm_stats_dotnet001
             }
         }
 
-        private List<GoogleAnalyticsEvent> _EventList;
+        private List<Event> _EventList;
 
-        private List<EventRow> events = new();
+        private List<EventRow> _ClickEvents = new();
+
+        private List<EventRow> _MailToEvents = new();
 
         private readonly ILogger<RetrieveAnalyticsData> _logger;
+
+        private Dictionary<string, int> _ClicksById; 
 
         public RetrieveAnalyticsData(ILogger<RetrieveAnalyticsData> logger)
         {
@@ -77,71 +98,99 @@ namespace appsvc_function_dev_cm_stats_dotnet001
             var config = new ConfigurationBuilder().AddJsonFile("appsettings.json", true, true).AddEnvironmentVariables().Build();
             string connectionString = config["AzureWebJobsStorage"];
             string containerName = config["containerName"];
-            string fileName = "GA4Report_Events_453422813_20241029163737.json";
+            string fileName = "GA4Report_Events_453422813_2024-11-08.json";
 
             var Getdata = GetBlob(connectionString, containerName, fileName, context);
-
+            
             var obj = JsonConvert.DeserializeObject<dynamic>(Getdata);
-            events = ((JArray)obj.Rows).ToObject<List<EventRow>>();
-            _logger.LogWarning($"events.Count = {events.Count}");
 
+
+            // where EventName = "click"
+
+            _ClickEvents = ((JArray)obj.Rows).ToObject<List<EventRow>>();
+            _ClickEvents.RemoveAll(x => x.EventName != "click");
+
+            _MailToEvents = new List<EventRow>(_ClickEvents);
+            _MailToEvents.RemoveAll(x => x.LinkUrl.IndexOf("mailto:") < 0);
+
+            //_logger.LogWarning($"events.Count = {_ClickEvents.Count}");
+            _logger.LogWarning($"events.Count = {_MailToEvents.Count}");
+
+            _logger.LogInformation(" ");
+            _logger.LogInformation(JsonConvert.SerializeObject(_MailToEvents));
             _logger.LogInformation(" ");
 
 
-            _EventList = new List<GoogleAnalyticsEvent>();
+            _ClicksById = new();
 
-            foreach (var e in events) {
-                //_logger.LogWarning($"e.DimensionValues.Count = {e.DimensionValues.Count}");
-                //foreach (var d in e.DimensionValues) {
-                //    _logger.LogWarning($"d.Value = {d.Value}");
-                //    //_logger.LogWarning($"d.HasValue = {d.HasValue}");
-                //    //_logger.LogWarning($"d.OneValueCase = {d.OneValueCase}");
-                //}
-                //_logger.LogInformation("------------------------------------------------------------");
-                //_logger.LogWarning($"e.MetricValues.Count = {e.MetricValues.Count}");
-                //foreach (var m in e.MetricValues)
-                //{
-                //    _logger.LogWarning($"d.ValueOtherName = {m.ValueOtherName}");
-                //    //_logger.LogWarning($"d.HasValue = {m.HasValue}");
-                //    //_logger.LogWarning($"d.OneValueCase = {m.OneValueCase}");
-                //}
+            //foreach (var e in _ClickEvents)
+            //{
+            //    if (e.LinkUrl.StartsWith("mailto:"))
+            //    {
+            //        //_logger.LogInformation($"e.DateHour = {e.DateHour}");
+            //        //_logger.LogInformation($"e.EventName = {e.EventName}");
+            //        //_logger.LogInformation($"e.LinkUrl = {e.LinkUrl}");
+            //        //_logger.LogInformation($"e.Id = {e.Id}");
 
-                _EventList.Add(new GoogleAnalyticsEvent(e.DimensionValues[0].Value, e.DimensionValues[1].Value, e.DimensionValues[2].Value));
+            //        if (_ClicksById.ContainsKey(e.Id))
+            //        {
+            //            _ClicksById[e.Id] = _ClicksById[e.Id] + 1;
+            //        }
+            //        else
+            //        {
+            //            _ClicksById.Add(e.Id, 1);
+            //        }
+            //    }
+            //}
 
-                //_logger.LogInformation(" ");
-            }
-
-
-            foreach (var e in _EventList)
+            foreach (var e in _MailToEvents)
             {
-                if (e.LinkUrl.StartsWith("mailto:"))
+                if (_ClicksById.ContainsKey(e.Id))
                 {
-                    _logger.LogInformation($"e.DateHour = {e.DateHour}");
-                    _logger.LogInformation($"e.EventName = {e.EventName}");
-                    _logger.LogInformation($"e.LinkUrl = {e.LinkUrl}");
+                    _ClicksById[e.Id] = _ClicksById[e.Id] + 1;
                 }
-
+                else
+                {
+                    _ClicksById.Add(e.Id, 1);
+                }
             }
 
+            _logger.LogInformation(" ");
 
+            var config2 = ReportConfiguration.GetConfig();
+            var strStartDateFromConfig = config2.DateConfiguration.StartDate;
+            var strEndDateFromConfig = config2.DateConfiguration.EndDate;
 
+            _logger.LogInformation($"For the period of time between {strStartDateFromConfig} and {strEndDateFromConfig}:");
+            _logger.LogInformation(" ");
 
+            foreach (var c in _ClicksById)
+            {
+                _logger.LogInformation($"Job Id {c.Key} was clicked {c.Value} times.");
+            }
 
+            _logger.LogInformation(" ");
 
+            //_EventList = new List<GoogleAnalyticsEvent>();
 
+            //foreach (var e in events) {
+            //    _EventList.Add(new GoogleAnalyticsEvent(e.DateHour, e.EventName, e.LinkUrl));
+            //}
 
+            //foreach (var e in _EventList)
+            //{
+            //    if (e.LinkUrl.StartsWith("mailto:"))
+            //    {
+            //        _logger.LogInformation($"e.DateHour = {e.DateHour}");
+            //        _logger.LogInformation($"e.EventName = {e.EventName}");
+            //        _logger.LogInformation($"e.LinkUrl = {e.LinkUrl}");
+            //    }
 
-
-
-
-
-
-
-
+            //}
 
             _logger.LogInformation("RetrieveAnalyticsData processed a request.");
 
-            return new OkResult();
+            return new OkObjectResult(JsonConvert.SerializeObject(_ClickEvents));
         }
 
         private string GetBlob(string connectionString, string containerName, string fileName, ExecutionContext executionContext)
